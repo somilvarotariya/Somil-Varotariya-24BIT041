30784a7887e24c2e9c4d22359c494d1f2ed54593
Somil Varotariya <somilvarotariya08@gmail.com>
2025-04-23 00:42:25 +0530

    Add files via upload

---
+++ 06/lec6Q6.py
@@ -0,0 +1,6 @@
+tup = (1, 2, 3, 4)
+print(tup)
+lis = list(tup)
+lis[1] = 20
+tup = tuple(lis)
+print(tup)
---
+++ 06/lecQ7.py
@@ -0,0 +1,5 @@
+tup = (1, 2, 3, 4, 5)
+lis = list(tup)
+lis.pop(2)  
+tup = tuple(lis)
+print(tup)
